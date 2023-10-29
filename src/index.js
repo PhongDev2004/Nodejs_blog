85556441c08508da2c8b936ad3c725552769971d
phonglb123 <luongbaphong20041@gmail.com>
2023-10-29 10:22:22 +0700

    install husky

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const { log } = require('console')
-const handlebars = require('express-handlebars').engine
-const app = express()
-const port = 3000
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const { log } = require("console");
+const handlebars = require("express-handlebars").engine;
+const app = express();
+const port = 3000;
 
-const route = require('./routes')
+const route = require("./routes");
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
-    express.urlencoded({
-        extended: true,
-    })
-)
-app.use(express.json())
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 
 // HTTP logger
 // app.use(morgan('combined'))
 
 // Template engine
 app.engine(
-    'hbs',
-    handlebars({
-        extname: '.hbs',
-    })
-)
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources/views'))
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 // console.log('PATH: ', path.join(__dirname, 'resources/views'));
 
 // Route init
-route(app)
+route(app);
 
 // 127.0.0.1 - localhost
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 // https://www.google.com/search?q=f8 lap trinh&ref=f8&author=phonglb
